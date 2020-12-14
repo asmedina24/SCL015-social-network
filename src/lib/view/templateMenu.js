03391640ef8f28bb4a2d936b3ba078ee21a83b57
alejandra-medina <alejandramedina1215@gmail.com>
2020-12-14 13:00:02 -0300

    google

--- src/lib/view/templateMenu.js
+++ src/lib/view/templateMenu.js
@@ -1,16 +1,12 @@
 export const menu = () => {
   const ViewMenu = `<ul>
-    <li>
       <a href="#/">Home</a>
-    </li>
-    <li>
-      <a href="#/login">Login</a>
-    </li>
-    <li>
+      
       <a href="#/register">Registro</a>
-    </li>
+      </ul>
+      
     
-  </ul>`;
+     `;
 
   return ViewMenu;
 };
--- src/main.js
+++ src/main.js
@@ -3,12 +3,18 @@
 import { myFunction } from './lib/index.js';
 import { menu } from './lib/view/templateMenu.js';
 import { changeroute } from './lib/router.js';
+import { home } from './lib/view/templateHome.js';
+import { register } from './lib/view/templateRegister.js';
 
 const init = () => {
+  document.getElementById('root1').appendChild(home());
   document.getElementById('root').innerHTML = menu();
   window.addEventListener('hashchange', () => {
+    document.getElementById('root1').innerHTML = '';
     myFunction();
     changeroute(window.location.hash);
   });
 };
 window.addEventListener('load', init);
+
+
