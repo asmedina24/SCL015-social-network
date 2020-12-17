ad7d5f0be0c7884e25f361fe569dcd122c5cb3f1
Caroline-Jeldres <cjeldresm@gmail.com>
2020-12-17 11:28:32 -0300

    nada

---
+++ src/lib/view/fiStore.js
@@ -0,0 +1,18 @@
+
+
+export const datosUser = {
+// Add a second document with a generated ID.
+ const db = firebase.firestore();
+db.collection("users").add({
+    first: "Alan",
+    middle: "Mathison",
+    last: "Turing",
+    born: 1912
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});
+}
