57fda8958e9d906bfcae0d45653d69357ec36e62
Caroline-Jeldres <cjeldresm@gmail.com>
2021-01-20 09:57:20 -0300

    ocultar menu

--- src/lib/view/templateMenu.js
+++ src/lib/view/templateMenu.js
@@ -1,33 +1,37 @@
+import contentLogin from '../function/login.js';
+
 export const menu = () => {
+  const ViewMenu = ` 
+  <nav class="navegacion">
+  <img class="img_infopet" src="./img/group_2.png">
+  <div id='nav' class="nav">   
+    <ul> 
+      <a href="#/home">Home</a>
+      <a href="#/register">Registro</a>
+    </ul> 
+  </div>
+  <div id='navLogin' class="navLogin">   
+    <button id="singOut" class="cerrar_sesion">Cerrar Sesion</button>
+  </div>
+  </nav>
+    
+     `;
   firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      if (user.id === true) {
-        const ViewMenu = ` 
-        <nav class="navegacion">
-        <img class="img_infopet" src="./img/group_2.png">
-        <div id='nav' class="nav">   
-          <ul> 
-            <a href="#/home">Home</a>
-            <a href="#/register">Registro</a>
-          </ul> 
-        </div>
-        <div id='nav_login'>   
-        <ul> 
-          <a href="#/home">Home</a>
-          <a href="#/register">Registro</a>
-          </ul> 
-         
-          </div>
-        </nav>
-          
-           `;
-        const nav = document.getElementById('nav');
-        nav.style.display = 'none';
-        return ViewMenu;
-      } else {
-        alert('verifica tu correo');
-      }
+    if (user !== null) {
+      const nav = document.getElementById('nav');
+      const navLogin = document.getElementById('navLogin');
+      nav.style.display = 'none';
+      navLogin.style.display = 'block';
+      const cerrar = document.querySelector(('#singOut'));
+      cerrar.addEventListener('click', () => {
+        contentLogin.cerrarsesion();
+      });
+    } else {
+      const nav = document.getElementById('nav');
+      const navLogin = document.getElementById('navLogin');
+      nav.style.display = 'block';
+      navLogin.style.display = 'none';
     }
   });
-  
+  return ViewMenu;
 };
